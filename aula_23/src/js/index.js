24591ab1e77f97a66dff10c8fc37623d2c492eec
Kelly Yuzawa <keandna@hotmail.com>
2022-09-11 07:37:35 -0300

    adição

--- aula_23/src/js/index.js
+++ aula_23/src/js/index.js
@@ -1,10 +1,2 @@
-import './../scss/index.scss'
-import img from './../image/imagem.png'
+import '../scss/styles.scss'
 
-function imageComponent(){
-    const elemImg = document.getElementById('imagemLogo')
-    elemImg.src = img
-    return elemImg
-}
-
-imageComponent()
--- aula_23/webpack.config.js
+++ aula_23/webpack.config.js
@@ -1,56 +1,76 @@
 const path = require('path')
 const HtmlWebpack = require('html-webpack-plugin')
 const MiniCssExtract = require('mini-css-extract-plugin')
+const HtmlWebpackLiveReload = require('html-webpack-live-reload-plugin')
+const ImageminPlugin = require('imagemin-webpack-plugin')
 
 module.exports = {
-    entry: './src/js/index.js', //local ao qual o arquivo será pego
-    output:{
-        filename: 'main.js', //insere um nome para o arquivo js
-        path: path.resolve(__dirname, 'dist') //cria diretório(pasta) dist
+
+    entry: './src/js/index.js',
+    devtool: 'eval-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test:/\.(sa|c|sc)ss$/,    // Expressão do Regex
-            use: [
-                MiniCssExtract.loader,
-                'css-loader',
-                'sass-loader'
-            ]
-        },{
-            test:/\.css$/i,
-            use: [
-                'style-loader',
-                'css-loader'
-            ] 
-        },{
-            test: /\.js$/i,
-            exclude: /node_modules/, //exclui o arquivo "node_modeles"
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets:['@babel/preset-env']
+        rules: [
+            {
+                test: /\.(sa|c|sc)ss$/i, 
+                use: [
+                    MiniCssExtract.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,  
+                dependency: { not: ['url'] },
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                        }
+                    },
+                ],
+                type: 'javascript/auto'
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
             }
-        },
-        {
-            test: /\.(jpeg|jpg|png|svg|gif)$/i,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]'
-                }
-            }
-        }
-    ]
+        ]
     },
     plugins: [
         new HtmlWebpack({
-            filename: 'index.html', //nome do arquivo criado
-            template: './src/index.html' // Local onde o arquivo será pego
+            template: './src/index.html',
+            filename: 'index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            }
         }),
+        new HtmlWebpackLiveReload(),
         new MiniCssExtract({
-            filename: 'style.css',
-            // template: './src/styles.scss'
+            filename: 'style.css'
         })
     ]
+
 }
