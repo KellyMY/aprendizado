da57920133369a4ce2443c5be54f2e5228c4c65e
Kelly Yuzawa <keandna@hotmail.com>
2023-01-08 17:19:21 -0300

    alterações

--- aula_33/testando/src/App.test.js
+++ aula_33/testando/src/App.test.js
@@ -1,7 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react";
+import '@testing-library/jest-dom';
 
 test('teste titulo', () => {
+  console.log('test')
   render(<App />);
   const testCase = document.querySelector('.App h1');
   expect(testCase).toHaveTextContent(/meu painel de controle/i);
